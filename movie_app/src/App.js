a876b9ddb3c8845b8c6b908c4b0048d5b032aef9
GeonLee <leegun2003@gmail.com>
2018-03-14 18:11:55 +0900

    (React JS Fundamentals #11): Practicing this setState

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,27 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title:"Matrix",
-    poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg"
-  },
-  {
-    title:"Full Metal Jacket",
-    poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BNzc2ZThkOGItZGY5YS00MDYwLTkyOTAtNDRmZWIwMGRhYTc0L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,656,1000_AL_.jpg"
-
-  },
-  {
-    title:"Oldboy",
-    poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BMTI3NTQyMzU5M15BMl5BanBnXkFtZTcwMTM2MjgyMQ@@._V1_.jpg",
-  },
-  {
-    title:"Star Wars",
-    poster:"https://blog.rebelpilgrim.com/wp-content/uploads/2017/10/7773769005_star-wars.jpg"
-  }
-]
-
 class App extends Component {
+
+  state = {
+    greeting : 'hello',
+    movies : [
+      {
+        title:"Matrix",
+        poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg"
+      },
+      {
+        title:"Full Metal Jacket",
+        poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BNzc2ZThkOGItZGY5YS00MDYwLTkyOTAtNDRmZWIwMGRhYTc0L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,656,1000_AL_.jpg"
+
+      },
+      {
+        title:"Oldboy",
+        poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BMTI3NTQyMzU5M15BMl5BanBnXkFtZTcwMTM2MjgyMQ@@._V1_.jpg",
+      },
+      {
+        title:"Star Wars",
+        poster:"https://blog.rebelpilgrim.com/wp-content/uploads/2017/10/7773769005_star-wars.jpg"
+      }
+    ]
+  }
 
   // Render : componentWillMount() -> render() -> componentDidMount()
   // Update : componentWillReceiveProps() -> shouldComponentUpdate() == true(if, different) -> componentWillUpdate() -> render() -> componentDidUpdate()
@@ -32,6 +35,17 @@ class App extends Component {
   }
 
   componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        movies:[
+          ...this.state.movies,
+          {
+            title: "Transformer",
+            poster: "https://upload.wikimedia.org/wikipedia/ko/e/e4/%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%98%81%ED%99%94.jpg"
+          }
+        ]
+      })
+    },5000)
   //  console.log('3. did mount');
   }
 
@@ -39,7 +53,8 @@ class App extends Component {
   //  console.log('2. did render');
     return (
       <div className="App">
-        {movies.map((movie, index) =>{
+        {this.state.greeting}
+        {this.state.movies.map((movie, index) =>{
           return <Movie title ={movie.title} poster = {movie.poster} key = {index}/>
         })}
       </div>
