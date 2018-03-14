8106db743c3c54beda4848e89a2bf6f89d52f350
GeonLee <leegun2003@gmail.com>
2018-03-14 17:36:31 +0900

    PropType & Map
    PropType & Map

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,30 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
-];
+const movies = [
+  {
+    title:"Matrix",
+    poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg"
+  },
+  {
+    title:"Full Metal Jacket",
+    poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BNzc2ZThkOGItZGY5YS00MDYwLTkyOTAtNDRmZWIwMGRhYTc0L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,656,1000_AL_.jpg"
 
-const movieImage = [
-  "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
-  "https://images-na.ssl-images-amazon.com/images/M/MV5BNzc2ZThkOGItZGY5YS00MDYwLTkyOTAtNDRmZWIwMGRhYTc0L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,656,1000_AL_.jpg",
-  "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI3NTQyMzU5M15BMl5BanBnXkFtZTcwMTM2MjgyMQ@@._V1_.jpg",
-  "https://blog.rebelpilgrim.com/wp-content/uploads/2017/10/7773769005_star-wars.jpg"
+  },
+  {
+    title:"Oldboy",
+    poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BMTI3NTQyMzU5M15BMl5BanBnXkFtZTcwMTM2MjgyMQ@@._V1_.jpg",
+  },
+  {
+    title:"Star Wars",
+    poster:"https://blog.rebelpilgrim.com/wp-content/uploads/2017/10/7773769005_star-wars.jpg"
+  }
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImage[0]}/>
-        <Movie title={movieTitles[1]} poster={movieImage[1]}/>
-        <Movie title={movieTitles[2]} poster={movieImage[2]}/>
-        <Movie title={movieTitles[3]} poster={movieImage[3]}/>
+        {movies.map((movie, index) =>{
+          return <Movie title ={movie.title} poster = {movie.poster} key = {index}/>
+        })}
       </div>
-    );
+    )
   }
 }
 
